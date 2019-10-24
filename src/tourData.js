c1ff67402d1a3522104f7da68d750fee42595aa4
alice <azhang@strongwell.com>
2019-10-24 11:14:56 -0400

    first commint

---
+++ src/tourData.js
@@ -0,0 +1,26 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.png",
+    name: "new york bridge tour",
+    info:
+      "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. "
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.png",
+    name: "paris lights tour",
+    info:
+      "Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture and designer boutiques along the Rue du Faubourg Saint-Honoré.New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. "
+  },
+  {
+    id: 3,
+    city: "tokyo",
+    img: "./img/tokyo.png",
+    name: "tokyo temple tour",
+    info:
+      "Tokyo, Japan’s busy capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples. The opulent Meiji Shinto Shrine is known for its towering gate and surrounding woods.  "
+  }
+];
